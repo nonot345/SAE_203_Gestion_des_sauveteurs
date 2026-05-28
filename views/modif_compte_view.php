5d6804906d003433e56260650178b413d9f0b5fd
armand <armandsourice@gmail.com>
2026-05-28 11:53:41 +0200

    Add files via upload

---
+++ views/modif_compte_view.php
@@ -0,0 +1,42 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>page de modification du compte</title>
+</head>
+<body>
+<header>
+<h2>Modifier un compte existant</h2>
+<nav>
+<a href="../index.html"><img src="../images/idea.png" alt="Retour vers la page d'accueil" /></a>
+<a href="../view/crea_compte_view.html"><img src="../images/crea_compte.png" alt="Aller à la page de création du compte" /></a>
+</nav> 
+</header>
+<table>
+    <tr>
+        <td>
+            <ul>
+                <li><p>test1</p></li>
+                <li><p>test2</p></li>
+                <li><p>test3</p></li>
+                <li><p>test4</p></li>
+            </ul>
+        </td>
+
+        <td>
+            	<p><input type="text" size="100" maxlength="100" name="name" placeholder="nom du sauveteur" /></p>
+             	<p><input type="text" size="100" maxlength="100" name="name" placeholder="prenom du sauveteur" /></p>
+            	<p><input type="text" size="100" maxlength="100" name="name" placeholder="role du sauveteur" /></p>
+            	<p><input type="text" size="100" maxlength="100" name="name" placeholder="département du sauveteur" /></p>
+					<p><input type="text" size="100" maxlength="100" name="name" placeholder="numéro de tel du sauveteur" /></p>
+            	<p><input type="text" size="100" maxlength="100" name="name" placeholder="login" /></p>            	
+               <p><input type="text" size="100" maxlength="100" name="name" placeholder="mot de passe" /></p>
+               <p><input type="submit" value="Enregistrer" /></p>
+        </td>
+    </tr>
+</table>
+</body>
+</html>
