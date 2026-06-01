bc1d906b1840122664b3147a989a6d8c3d95b058
ryana <ryana.dealmeida1@gmail.com>
2026-06-01 11:28:42 +0200

    lecture_page_view1

---
+++ views/lecture_page.php
@@ -0,0 +1,63 @@
+<?php
+    require('views/header.php');
+?>
+
+<form action="index.php?route=add_equipment" method="post">
+<table border="1" cellpadding="8" cellspacing="0">
+    <thead>
+        <tr>
+            <th>Personnes</th>
+            <th> - </th>
+            <th> - </th>
+            <th> - </th>
+            <th> - </th>
+            <th> - </th>
+            <th> - </th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td><input type="text" size="15" maxlength="100"/></td>
+            <td><input type="text" size="15" maxlength="100"/></td>
+            <td><input type="text" size="15" maxlength="100"/></td>
+            <td><input type="text" size="15" maxlength="100"/></td>
+            <td><input type="text" size="15" maxlength="100"/></td>
+            <td><input type="text" size="15" maxlength="100"/></td>
+            <td><input type="text" size="15" maxlength="100"/></td>           
+        </tr>
+        <tr>
+            <td><input type="text" size="15" maxlength="100"/></td>
+            <td><input type="text" size="15" maxlength="100"/></td>
+            <td><input type="text" size="15" maxlength="100"/></td>
+            <td><input type="text" size="15" maxlength="100"/></td>
+            <td><input type="text" size="15" maxlength="100"/></td>
+            <td><input type="text" size="15" maxlength="100"/></td>
+            <td><input type="text" size="15" maxlength="100"/></td>
+        </tr>
+        <tr>
+            <td><input type="text" size="15" maxlength="100"/></td>
+            <td><input type="text" size="15" maxlength="100"/></td>
+            <td><input type="text" size="15" maxlength="100"/></td>
+            <td><input type="text" size="15" maxlength="100"/></td>
+            <td><input type="text" size="15" maxlength="100"/></td>
+            <td><input type="text" size="15" maxlength="100"/></td>
+            <td><input type="text" size="15" maxlength="100"/></td>
+        </tr>
+        
+         <tr>
+            <td><input type="text" size="15" maxlength="100"/></td>
+            <td><input type="text" size="15" maxlength="100"/></td>
+            <td><input type="text" size="15" maxlength="100"/></td>
+            <td><input type="text" size="15" maxlength="100"/></td>
+            <td><input type="text" size="15" maxlength="100"/></td>
+            <td><input type="text" size="15" maxlength="100"/></td>
+            <td><input type="text" size="15" maxlength="100"/></td>
+        </tr>
+        
+    </tbody>
+</table>
+</form>
+
+<?php
+    require('views/footer.php');
+?>
