0c349e7aed04b73eec7c1d51bb96518ffc6cb241
Poty Thomas <thomaspoty6@gmail.com>
2026-06-10 19:25:57 +0200

    finition authentification

--- controllers/auth_ctrl2.php
+++ controllers/auth_ctrl2.php
@@ -10,7 +10,6 @@ function login_ctrl() {
         login_form_ctrl($ask_route);
     }
 }
-
 function verify_login_ctrl(?string $route) {
     require('models/connection.php');
     require('models/user_crud.php');
@@ -23,32 +22,30 @@ function verify_login_ctrl(?string $route) {
         $_SESSION['login'] = $user['login'];
         $_SESSION['role']  = $user['type'];
         if ($route) {
-    header('Location: index.php?route=' . $route);
-} else {
-    switch ($user['type']) {
-        case 'admin':
-            header('Location: index.php?route=admin');
-            break;
-        case 'lecteur':
-            header('Location: index.php?route=operations');
-            break;
-        default:
-            header('Location: index.php');
-            break;
-    }
-}
+            header('Location: index.php?route=' . $route);
+        } else {
+            switch ($user['type']) {
+                case 'admin':
+                    header('Location: index.php?route=admin');
+                    break;
+                case 'administration':
+                    header('Location: index.php?route=operations');
+                    break;
+                default:
+                    header('Location: index.php');
+                    break;
+            }
+        }
         exit;
     } else {
         echo 'Erreur d\'authentification.';
         exit;
     }
 }
-
 function login_form_ctrl(?string $route) {
     require('views/login_views.php');
     login_form_view($route);
 }
-
 function logout_ctrl() {
     session_unset();
     session_destroy();
