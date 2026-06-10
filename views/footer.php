34fb19f23ade020730c82082bcce3d57a71f9a06
Noah <noah.roy28@yahoo.com>
2026-06-10 22:56:33 +0200

    modif footer

--- views/footer.php
+++ views/footer.php
@@ -2,7 +2,7 @@
 
 <footer>
     <span>Spéléo-Secours Français - Application de gestion des sauveteurs</span>
-    <span>&copy; 2025 - Tous droits réservés</span>
+    <span>&copy; 2026 - Tous droits réservés</span>
 </footer>
 
 </body>
